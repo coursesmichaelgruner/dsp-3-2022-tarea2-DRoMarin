9d361db8ff3a8bd0b3b93cbcbd0b3dd1e05ff7a9
Daniel Rojas <159dram@gmail.com>
2022-09-28 21:48:00 -0600

    clean up, no reproduction yet nor parameter parsing

--- play440.py
+++ play440.py
@@ -1,16 +1,14 @@
 import numpy as np
 import wave
 import matplotlib.pyplot as plt
-n = np.linspace(0,1,44100)
-y = (2**15-1)*np.sin(2*np.pi*440.0*n)
 
-sound = wave.open('note.wav', 'w')
-print(len(y))
-print(y)
-plt.plot(n,y)
-plt.show()
+Fs = 8000
+
+n = np.linspace(0,1,Fs)
+y = (2**15-1)*np.sin(2*np.pi*440.0*n)
 y = y.astype("<h")
+sound = wave.open('note.wav', 'w')
 sound.setnchannels(1)
 sound.setsampwidth(2)
-sound.setframerate(44100)
+sound.setframerate(Fs)
 sound.writeframesraw(y)
