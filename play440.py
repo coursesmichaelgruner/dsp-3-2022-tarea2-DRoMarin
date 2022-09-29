b0e6db25d030fae2a1b9697d42dd5f8210bf09f3
Daniel Rojas <159dram@gmail.com>
2022-09-29 14:23:21 -0600

    adds audio reproduction

--- play440.py
+++ play440.py
@@ -1,14 +1,17 @@
 import numpy as np
 import wave
 import matplotlib.pyplot as plt
+from playsound import playsound
 
 Fs = 8000
 
 n = np.linspace(0,1,Fs)
 y = (2**15-1)*np.sin(2*np.pi*440.0*n)
 y = y.astype("<h")
-sound = wave.open('note.wav', 'w')
-sound.setnchannels(1)
-sound.setsampwidth(2)
-sound.setframerate(Fs)
-sound.writeframesraw(y)
+with wave.open('note.wav', 'w') as sound:
+    sound.setnchannels(1)
+    sound.setsampwidth(2)
+    sound.setframerate(Fs)
+    sound.writeframesraw(y)
+
+playsound('note.wav')
